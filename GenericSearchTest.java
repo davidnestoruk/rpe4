fba3904945a21b740241ea03c597372a92aafbd7
David Nestoruk <davidnestoruk@users.noreply.github.com>
2014-03-02 15:11:45 +0000

    Update GenericSearchTest.java
    Changed move types.

--- GenericSearchTest.java
+++ GenericSearchTest.java
@@ -4,18 +4,18 @@ import rp13.search.interfaces.SuccessorFunction;
 import rp13.search.problem.puzzle.EightPuzzle;
 import rp13.search.problem.puzzle.EightPuzzle.PuzzleMove;
 import rp13.search.problem.puzzle.EightPuzzleSuccessorFunction;
+import rp13.search.util.ActionStatePair;
 import stringJumble.StringJumble;
-import stringJumble.StringJumble.StringMove;
+import stringJumble.StringJumbleMove;
 import stringJumble.StringJumbleSuccessorFunction;
 import agendas.AgendaQueue;
-import agendas.AgendaStack;
 
 public class GenericSearchTest {
 	
 	public static void main(String[] args) {
 		
-		//EightPuzzleTest();
-		StringJumbleTest();
+		EightPuzzleTest();
+		//StringJumbleTest();
 		
 	}
 	
@@ -23,20 +23,20 @@ public class GenericSearchTest {
 	 * Tests searches on the StringJumble class.
 	 */
 	private static void StringJumbleTest() {
-		String word = "java";
+		String word = "test";
 		
 		StringJumble startState = StringJumble.randomString(word);
 		StringJumble goalState = StringJumble.orderedString(word);
-		SuccessorFunction<StringMove, StringJumble> sf = new StringJumbleSuccessorFunction();
+		SuccessorFunction<StringJumbleMove, StringJumble> sf = new StringJumbleSuccessorFunction();
 		
 		/* BREADTH-FIRST-SEARCH */
-		AgendaQueue<StringJumble> agenda = new AgendaQueue<StringJumble>();
+		AgendaQueue<ActionStatePair<StringJumbleMove, StringJumble>> agenda = new AgendaQueue<ActionStatePair<StringJumbleMove, StringJumble>>();
 		
 		/* DEPTH FIRST SEARCH */
-		//AgendaStack<StringJumble> agenda = new AgendaStack<StringJumble>();
+		//AgendaStack<ActionStatePair<StringJumbleMove, StringJumble>> agenda = new AgendaStack<ActionStatePair<StringJumbleMove, StringJumble>>();
 		
-		UninformedSearch<StringJumble, StringMove> ui;
-		ui = new UninformedSearch<StringJumble, StringMove>(startState, goalState, agenda, sf);
+		UninformedSearch<StringJumble, StringJumbleMove> ui;
+		ui = new UninformedSearch<StringJumble, StringJumbleMove>(startState, goalState, agenda, sf);
 		ui.search();
 	}
 	
@@ -49,10 +49,10 @@ public class GenericSearchTest {
 		SuccessorFunction<PuzzleMove, EightPuzzle> sf = new EightPuzzleSuccessorFunction();
 		
 		/* BREADTH-FIRST-SEARCH */
-		//AgendaQueue<EightPuzzle> agenda = new AgendaQueue<EightPuzzle>();
+		AgendaQueue<ActionStatePair<PuzzleMove, EightPuzzle>> agenda = new AgendaQueue<ActionStatePair<PuzzleMove, EightPuzzle>>();
 		
 		/* DEPTH FIRST SEARCH */
-		AgendaStack<EightPuzzle> agenda = new AgendaStack<EightPuzzle>();
+		//AgendaStack<ActionStatePair<PuzzleMove, EightPuzzle>> agenda = new AgendaStack<ActionStatePair<PuzzleMove, EightPuzzle>>();
 		
 		UninformedSearch<EightPuzzle, PuzzleMove> ui;
 		ui = new UninformedSearch<EightPuzzle, PuzzleMove>(startState, goalState, agenda, sf);
