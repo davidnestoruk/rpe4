d8024411028901294b26e8e988dedcb52fea45fd
David Nestoruk <davidnestoruk@users.noreply.github.com>
2014-03-02 14:58:23 +0000

    Update StringJumbleSuccessorFunction.java
    No longer uses enumeration moves, now uses nested for loops to test every possible move.

--- StringJumbleSuccessorFunction.java
+++ StringJumbleSuccessorFunction.java
@@ -4,14 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import rp13.search.interfaces.SuccessorFunction;
-import rp13.search.problem.puzzle.EightPuzzle;
-import rp13.search.problem.puzzle.EightPuzzleSuccessorFunction;
-import rp13.search.problem.puzzle.EightPuzzle.PuzzleMove;
 import rp13.search.util.ActionStatePair;
-import stringJumble.StringJumble.StringMove;
 
 public class StringJumbleSuccessorFunction implements
-		SuccessorFunction<StringMove, StringJumble> {
+		SuccessorFunction<StringJumbleMove, StringJumble> {
 
 	/**
 	 * 
@@ -21,39 +17,47 @@ public class StringJumbleSuccessorFunction implements
 	 */
 	
 	public void getSuccessors(StringJumble _state,
-			List<ActionStatePair<StringMove, StringJumble>> _successors) {
+			List<ActionStatePair<StringJumbleMove, StringJumble>> _successors) {
 
 		assert (_successors != null);
+		int stringLength = _state.toString().length();
 
-		// for each of the moves that are available
-		for (StringMove move : StringMove.values()) {
-
-			// create a copy of the input state as we don't want to change
-			// it
-			StringJumble successor = new StringJumble(_state.toString());
-			// apply the move
-			successor.makeMove(move);
-			// store the move and action together in a pair and add to
-			// successor list
-			_successors
-					.add(new ActionStatePair<StringJumble.StringMove, StringJumble>(
-							move, successor));
+		for(int i=0; i<stringLength; i++) {
+			for(int j=i; j<stringLength; j++) {
+				if(i == j) {
+					continue;
+				}
+				
+				StringJumbleMove move = new StringJumbleMove(i, j);
+				
+				// create a copy of the input state as we don't want to change
+				// it
+				StringJumble successor = new StringJumble(_state.toString());
+				// apply the move
+				successor.makeMove(i, j);
+				// store the move and action together in a pair and add to
+				
+				// successor list
+				_successors
+						.add(new ActionStatePair<StringJumbleMove, StringJumble>(
+								move, successor));
+			}
 		}
 
 	}
 	
 	public static void main(String[] args) {
-		StringJumble state = StringJumble.orderedString("java");
+		StringJumble state = StringJumble.orderedString("hello");
 		StringJumbleSuccessorFunction successorFn = new StringJumbleSuccessorFunction();
 
 		// successors are added to the end of this list by the successor
 		// function
-		List<ActionStatePair<StringMove, StringJumble>> successors = new ArrayList<ActionStatePair<StringMove, StringJumble>>();
+		List<ActionStatePair<StringJumbleMove, StringJumble>> successors = new ArrayList<ActionStatePair<StringJumbleMove, StringJumble>>();
 		;
 		// get the successors of the given state
 		successorFn.getSuccessors(state, successors);
 
-		for (ActionStatePair<StringMove, StringJumble> successor : successors) {
+		for (ActionStatePair<StringJumbleMove, StringJumble> successor : successors) {
 
 			System.out.println("Applied move " + successor.getAction()
 					+ " and got:");
